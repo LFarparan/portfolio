81b4f466d6c6bf9e18b131faf963061bf19f34dd
Leonard Farparan <aguinaldo.farparan@gmail.com>
2024-03-31 00:21:30 +0800

    Added header greetings and flexing of contents

--- assets/script/portfolio.js
+++ assets/script/portfolio.js
@@ -1,14 +1,24 @@
 const root = document.querySelector(':root');
-const theme = document.querySelector('.theme');
 const blobcon = document.querySelector('.relablob');
+const themecon = document.querySelector('#theme-change')
+const themeIcon = document.querySelector('.theme-icon')
+const theme = document.querySelector('.theme')
+
 root.classList = 'cold';
 
+themecon.addEventListener('click', toggleTheme);
 theme.addEventListener('click', toggleTheme);
+themecon.addEventListener('mouseover', toggleIcon);
+themecon.addEventListener('mouseout', toggleIcon);
+
+
+function toggleIcon(){
+    themeIcon.classList.toggle('darken')
+}
 
 
 function toggleTheme(){
     root.classList.toggle('warm');
+    themeIcon.src = (themeIcon.src.includes('snowflake.svg'))? './assets/images/fire.svg': './assets/images/snowflake.svg';
     theme.src = (theme.src.includes('snowflake.svg'))? './assets/images/fire.svg': './assets/images/snowflake.svg'
-    theme.classList.toggle('.scale');
-
 }
