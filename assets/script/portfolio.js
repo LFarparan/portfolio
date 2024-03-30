9b90da1b873d600c5b1eed50fe8f0dc912156f32
Leonard Farparan <aguinaldo.farparan@gmail.com>
2024-03-30 16:29:26 +0800

    Added a theme toggle from cold to warm

--- assets/script/portfolio.js
+++ assets/script/portfolio.js
@@ -1,2 +1,14 @@
 const root = document.querySelector(':root');
+const theme = document.querySelector('.theme');
+const blobcon = document.querySelector('.relablob');
 root.classList = 'cold';
+
+theme.addEventListener('click', toggleTheme);
+
+
+function toggleTheme(){
+    root.classList.toggle('warm');
+    theme.src = (theme.src.includes('snowflake.svg'))? './assets/images/fire.svg': './assets/images/snowflake.svg'
+    
+
+}
